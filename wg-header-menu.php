ff19cd142756c04203cb532cb9671be4f1ee948b
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-04-28 23:43:03 -0500

    Home: conectando lista de menu inferior con BD

--- wg-header-menu.php
+++ wg-header-menu.php
@@ -1,41 +1,21 @@
+<?php
+//CATEGORIA
+$rst_categoria=mysql_query("SELECT * FROM lndd_noticia_categoria ORDER BY orden ASC;", $conexion);
+?>
 <nav class="clearfix">
 
 	<a href="" id="menu-button"><i class="fa fa-bars"></i></a>
 
 	<ul class="menu col-md-9 list-inline">
-		<li><a href="fashion.html">Política</a></li>
-		<li class="dropdown-submenu">
-			<a href="travel.html">Actualidad</a>
-			<ul class="dropdown-menu">
-				<li><a href="">xxx1</a></li>
-				<li class="dropdown-submenu">
-					<a href="">xxxxx2</a>
-					<ul class="dropdown-menu">
-						<li><a href="">xxxx3</a></li>
-						<li><a href="">xxxx4</a></li>
-						<li><a href="">xxxx5</a></li>
-					</ul>
-				</li>
-				<li><a href="">xxxxx6/a></li>
-				<li><a href="">xxxx7</a></li>
-				<li><a href="">xxxxx8</a></li>
-			</ul>
-		</li>
-		<li><a href="fashion.html">Economía</a></li>
-		<li><a href="travel.html">Tecnología</a></li>
-		<li class="dropdown-submenu">
-			<a href="fashion.html">Mundo</a>
-			<ul class="dropdown-menu">
-				<li><a href="">xxxxx1</a></li>
-				<li><a href="">Mxxxx2</a></li>
-				<li><a href="">xxxxx3</a></li>
-				<li><a href="">xxxx4</a></li>
-				<li><a href="">xxxxx5</a></li>
-			</ul>
-		</li>
-		<li><a href="travel.html">Sociedad y Cultura</a></li>
-		<li></li>
-		<li></li>
+		<?php while($fila_categoria=mysql_fetch_array($rst_categoria)){
+				$categoria_url=$fila_categoria["url"];
+				$categoria_titulo=$fila_categoria["categoria"];
+
+				//URL
+				$categoria_UrlWeb=$web."seccion/".$categoria_url;
+		?>
+		<li><a href="<?php echo $categoria_UrlWeb; ?>"><?php echo $categoria_titulo; ?></a></li>
+		<?php } ?>
 	</ul>
 	<form class="form-search col-md-3">
 		<div class="input-group">
