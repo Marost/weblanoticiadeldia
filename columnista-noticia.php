a2079066d3ad8abdb5f179ab87c400ff827c76a7
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-05-19 10:53:32 -0500

    Home: creacion de seccion de columnista y noticia de columnista

---
+++ columnista-noticia.php
@@ -0,0 +1,133 @@
+<?php
+require_once("panel@lndd/conexion/conexion.php");
+require_once("panel@lndd/conexion/funciones.php");
+
+//VARIABLES DE URL
+$ReqId=$_REQUEST["id"];
+$ReqUrl=$_REQUEST["url"];
+
+//NOTICIA
+$rst_nota=mysql_query("SELECT * FROM lndd_columnista_columna WHERE id=$ReqId;", $conexion);
+$fila_nota=mysql_fetch_array($rst_nota);
+
+//VARIABLES
+$not_titulo=$fila_nota["titulo"];
+$not_contenido=$fila_nota["contenido"];
+$nota_fechaFinal=$fila_nota["fecha"];
+$nota_columnista=$fila_nota["columnista"];
+
+//COLUMNISTA
+$rst_usuario=mysql_query("SELECT * FROM lndd_columnista WHERE id=$nota_columnista");
+$fila_usuario=mysql_fetch_array($rst_usuario);
+
+//VARIABLES
+$user_nomCompleto=$fila_usuario["nombre"]." ".$fila_usuario["apellidos"];
+$user_url=$fila_usuario["url"];
+$user_UrlWeb=$web."columnista/".$user_url;
+
+/*
+$not_imagen=$fila_nota["imagen"];
+$not_imagen_carpeta=$fila_nota["imagen_carpeta"];
+$not_categoria=$fila_nota["categoria"];
+$not_usuario=$fila_nota["usuario"];
+*/
+
+/*
+//GALERIA DE FOTOS
+$rst_notaFotos=mysql_query("SELECT * FROM lndd_noticia_slide WHERE noticia=$ReqId ORDER BY orden ASC", $conexion);
+$num_notaFotos=mysql_num_rows($rst_notaFotos);
+*/
+
+//URLS
+$not_web=$web."columna/".$ReqId."-".$ReqUrl;
+$not_web_img=$web."imagenes/upload/".$not_imagen_carpeta."".$not_imagen;
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $not_titulo; ?></title>
+	
+	<?php require_once("wg-header-script.php"); ?>
+
+</head>
+<body>
+	<!--HEADER-->
+	<?php require_once("wg-header.php"); ?>
+	<!--END HEADER-->
+
+	<!--CONTAINER-->
+	<div class="container">
+		<!--MENU-->
+		<?php require_once("wg-header-menu.php"); ?>
+		<!--END MENU-->
+		
+		<!--MAIN SECTION-->
+		<div class="main post-page">
+			<div class="row">
+				<!--CONTENT-->
+				<div class="col-md-9 col-sm-12 clearfix author">
+					
+					<h2>Columnista: <a href="<?php echo $user_UrlWeb; ?>"><span><?php echo $user_nomCompleto; ?></span></a></h2>
+
+					<!--POST-->
+					<article class="post mid fullwidth">
+					
+						<div class="row">
+							<div class="info col-md-12 col-sm-12">
+								<div class="info">
+									<h1><?php echo $not_titulo; ?></h1>
+									<div class="data">
+										<p class="details"><?php echo $nota_fechaFinal; ?></p>
+									</div>
+									<div class="text">
+										<?php echo $not_contenido; ?>
+									</div>
+										
+								</div>
+							</div>
+						</div>
+
+					</article>
+					<!--END POST-->
+				
+					<!-- COMENTARIOS -->
+					<div class="row">
+						<h3>Comentarios</h3>
+
+						<div id="fb-root"></div>
+						<script>(function(d, s, id) {
+						  var js, fjs = d.getElementsByTagName(s)[0];
+						  if (d.getElementById(id)) return;
+						  js = d.createElement(s); js.id = id;
+						  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=258988607636876";
+						  fjs.parentNode.insertBefore(js, fjs);
+						}(document, 'script', 'facebook-jssdk'));</script>
+
+						<div class="fb-comments" data-href="<?php echo $not_web; ?>" data-numposts="5" data-colorscheme="light"></div>
+					</div>					
+					<!-- FIN COMENTARIOS -->
+					
+				</div>
+				<!--END CONTENT-->
+
+				<!--SIDEBAR-->
+				<?php require_once("wg-sidebar.php"); ?>
+				<!--END SIDEBAR-->
+			</div>	
+		</div>
+		<!--END MAIN SECTION-->
+		
+		<!--FOOTER-->
+		<?php require_once("wg-footer.php"); ?>
+		<!--END FOOTER-->
+
+	</div>
+	<!--END CONTAINER-->
+
+<?php require_once("wg-footer-script.php"); ?>
+
+</body>
+</html>
---
+++ columnista.php
@@ -0,0 +1,143 @@
+<?php
+require_once("panel@lndd/conexion/conexion.php");
+require_once("panel@lndd/conexion/funciones.php");
+
+//VARIBALES DE URL
+$ReqUrl=$_REQUEST["url"];
+
+//COLUMNISTA SELECCIONADO
+$rst_columnista=mysql_query("SELECT * FROM lndd_columnista WHERE url='$ReqUrl'", $conexion);
+$fila_columnista=mysql_fetch_array($rst_columnista);
+
+//VARIABLES
+$Columista_id=$fila_columnista["id"];
+$Columista_url=$fila_columnista["url"];
+$Columista_nombre=$fila_columnista["nombre_completo"];
+$Columnista_descripcion=soloDescripcion($fila_columnista["descripcion"]);
+$Columnista_imagen=$fila_columnista["foto"];
+
+//COLUMNAS
+$rst_columnas=mysql_query("SELECT * FROM lndd_columnista_columna WHERE columnista=$Columista_id ORDER BY fecha DESC", $conexion);
+
+//URLs
+$Columnista_UrlImg=$web."imagenes/columnistas/".$Columnista_imagen;
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="initial-scale=1.0,width=device-width">
+	<title>MODARAMA</title>
+	
+	<?php require_once("wg-header-script.php"); ?>
+
+</head>
+<body>
+
+	<!--HEADER-->
+	<?php require_once("wg-header.php"); ?>
+	<!--END HEADER-->
+
+	<!--CONTAINER-->
+	<div class="container">
+		<!--MENU-->
+		<?php require_once("wg-header-menu.php"); ?>
+		<!--END MENU-->
+
+		<!--MAIN SECTION-->
+		<div class="main">
+			<div class="row">
+				<!--CONTENT-->
+				<div class="col-md-9 col-sm-12 author">
+					<h2>Columnista: <span><?php echo $Columista_nombre; ?></span></h2>
+					<div class="row">
+						<article class="col-md-4 col-sm-4 mid member">
+							<div class="img">
+								<img src="<?php echo $Columnista_UrlImg; ?>" alt="post2">
+							</div>
+							<div class="info">
+								<p class="tags">
+									<a href="">Founder</a>
+									<a href="">Ceo</a>
+								</p>
+								<h1><?php echo $Columista_nombre; ?></h1>
+								<p class="text">
+									<?php echo $Columnista_descripcion; ?>
+								</p>
+							</div>
+							<ul class="social list-inline">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#"><i class="fa  fa-tumblr"></i></a></li>
+							</ul>
+						</article>
+						<!--POSTS-->	
+						<div class="posts col-md-8 col-sm-8">
+							<h3>Columnas</h3>
+
+							<?php while($fila_columna=mysql_fetch_array($rst_columnas)){
+									$columna_id=$fila_columna["id"];
+									$columna_url=$fila_columna["url"];
+									$columna_titulo=$fila_columna["titulo"];
+									$columna_contenido=soloDescripcion($fila_columna["contenido"]);
+									$columna_fecha=$fila_columna["fecha"];
+
+									//URLs
+									$Columna_UrlWeb=$web."columna/".$columna_id."-".$columna_url;
+							?>
+							<article class="row mid">
+
+								<div class="img">
+									<img src="img/mid2.jpg" alt="post">
+								</div>
+
+								<div class="info">
+									<h1><a href="<?php echo $Columna_UrlWeb; ?>"><?php echo $columna_titulo; ?></a></h1>
+
+									<p class="details"><?php echo $columna_fecha; ?></p>
+
+									<p class="text">
+										<?php echo $columna_contenido; ?>
+									</p>
+									<a href="<?php echo $Columna_UrlWeb; ?>" class="btn btn-default">Leer más...</a>
+									
+								</div>
+							</article>
+							<?php } ?>
+
+						  	<ul class="pagination">
+						    	<li class="disabled"><span><i class="fa fa-long-arrow-left"></i></span></li>
+						    	<li class="active"><span>1</span></li>
+						    	<li><a href="#">2</a></li>
+						    	<li><a href="#">3</a></li>
+						    	<li><a href="#">...</a></li>
+						    	<li><a href="#">15</a></li>
+						    	<li><a href="#"><i class="fa fa-long-arrow-right"></i></a></li>
+						 	</ul>
+						</div>
+						<!--END POSTS-->
+					</div>
+				</div>
+				<!--END CONTENT-->
+
+				<!--SIDEBAR-->
+				<?php require_once("wg-sidebar.php"); ?>
+				<!--END SIDEBAR-->
+
+			</div>	
+		</div>
+		<!--END MAIN SECTION-->
+
+		<!--FOOTER-->
+		<?php require_once("wg-footer.php"); ?>
+		<!--END FOOTER-->
+
+	</div>
+	<!--END CONTAINER-->
+
+<?php require_once("wg-footer-script.php"); ?>
+
+</body>
+</html>
