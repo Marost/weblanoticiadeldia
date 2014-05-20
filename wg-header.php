ae6f2f4f898ea82e0509e32194806f9ee7e98cfe
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-05-19 21:01:59 -0500

    Home: Cambio de lista de menu superior

--- wg-header.php
+++ wg-header.php
@@ -7,23 +7,9 @@
 			<a href="" id="header-menu-button"><i class="fa fa-bars"></i></a>  
 			<ul class="list-inline">
 				<li class="/"><a href="">Inicio</a></li>
-				<li><a href="#">Director</a></li>
-				<li class="dropdown-submenu"><a href="">Columnistas</a>
-					<ul class="dropdown-menu">
-						<li><a href="search-#">Search results</a></li>
-						<li><a href="#">Author page</a></li>
-						<li><a href="#">Error404</a></li>
-						<li><a href="#">Coming soon</a></li>
-						<li><a href="#">Category page (grid)</a></li>
-						<li><a href="#">Category page (list)</a></li>
-						<li><a href="video-#">Post with video</a></li>
-						<li><a href="slider-#">Post with slider</a></li>
-						<li><a href="small-#">Post</a></li>
-						<li><a href="fullwidth-post-#">Post fullwidth with sidebar</a></li>
-					</ul>
-				</li>
+				<li><a href="#">Publicidad</a></li>
 				<li><a href="#">Contacto</a></li>
-				<li><a href="#">Nosotros</a></li>
+				<li><a href="nosotros">Nosotros</a></li>
 			</ul>
 		</nav>
 		<!--END MENU-->
