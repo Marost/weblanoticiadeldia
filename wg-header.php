4896ba505f7e87c3337dbe943a4d32a3ebbbe0f1
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-05-20 00:58:47 -0500

    Home: agregando separacion de header y publicidad Adsense

--- wg-header.php
+++ wg-header.php
@@ -13,5 +13,22 @@
 			</ul>
 		</nav>
 		<!--END MENU-->
+
+		<div class="sepheader">
+			<style>
+			.pp-responsive { width: 320px; height: 50px; }
+			@media(min-width: 500px) { .pp-responsive { width: 468px; height: 60px; } }
+			@media(min-width: 800px) { .pp-responsive { width: 728px; height: 90px; } }
+			</style>
+			<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+			<!-- PP - Responsive -->
+			<ins class="adsbygoogle pp-responsive"
+			     style="display:inline-block"
+			     data-ad-client="ca-pub-3674889010429322"
+			     data-ad-slot="1879845946"></ins>
+			<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
 	</div>
 </header>
