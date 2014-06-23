24a50c0f5a2055613958bd0aae2fe5b1cac10e7f
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-06-23 12:31:11 -0500

    Home: agregando opcion de "Descripcion de la nota"
    - Open graph
    - Titulo y descripcion, encima de imagen principal
    - Mostrar datos de Reportero Ciudadano

--- noticia.php
+++ noticia.php
@@ -12,12 +12,14 @@ $fila_nota=mysql_fetch_array($rst_nota);
 
 //VARIABLES
 $not_titulo=$fila_nota["titulo"];
+$not_contenido_corto=$fila_nota["contenido_corto"];
 $not_contenido=$fila_nota["contenido"];
 $not_imagen=$fila_nota["imagen"];
 $not_imagen_carpeta=$fila_nota["imagen_carpeta"];
 $not_video=$fila_nota["video"];
 $not_categoria=$fila_nota["categoria"];
 $not_usuario=$fila_nota["usuario"];
+$not_redaccion=$fila_nota["redaccion"];
 
 //FECHA PUBLICACION
 if($fila_nota["fecha_publicacion"]<>"0000-00-00 00:00:00"){
@@ -30,12 +32,20 @@ if($fila_nota["fecha_publicacion"]<>"0000-00-00 00:00:00"){
     $nota_fechaFinal=$fila_nota["fecha"];
 }
 
+//CATEGORIA
+$rst_categoria=mysql_query("SELECT * FROM lndd_noticia_categoria WHERE id=$not_categoria", $conexion);
+$fila_categoria=mysql_fetch_array($rst_categoria);
+
+//VARIABLES
+$cat_titulo=$fila_categoria["categoria"];
+
 //USUARIO
-$rst_usuario=mysql_query("SELECT usuario, nombre, apellidos FROM lndd_usuario WHERE usuario='$not_usuario'");
+$rst_usuario=mysql_query("SELECT * FROM lndd_rciud_usuario_datos WHERE usuario='$not_usuario'", $conexion);
 $fila_usuario=mysql_fetch_array($rst_usuario);
 
 //VARIABLES
 $user_nomCompleto=$fila_usuario["nombre"]." ".$fila_usuario["apellidos"];
+$user_descripcion=soloDescripcion($fila_usuario["descripcion"],150);
 
 //GALERIA DE FOTOS
 $rst_notaFotos=mysql_query("SELECT * FROM lndd_noticia_slide WHERE noticia=$ReqId ORDER BY orden ASC", $conexion);
@@ -54,6 +64,16 @@ $not_web_img=$web."imagenes/upload/".$not_imagen_carpeta."".$not_imagen;
 	<title><?php echo $not_titulo; ?> | <?php echo $web_nombre; ?></title>
 	
 	<?php require_once("wg-header-script.php"); ?>
+
+	<!-- Open Graph -->
+    <meta property="og:type" content='article' >
+    <meta property="og:site_name" content='<?php echo $web_nombre; ?>' >
+    <meta property="og:title" content='<?php echo $not_titulo; ?>'>
+    <meta property="og:description" content='<?php echo $not_contenido_corto; ?>'>
+    <meta property="og:url" content='<?php echo $not_web; ?>' >
+    <meta property="og:image" content='<?php echo $not_web_img; ?>' >
+    <meta property="fb:admins" content='1434798696787255'>
+    <!-- fin Open Graph -->
 
 </head>
 <body>
@@ -74,6 +94,16 @@ $not_web_img=$web."imagenes/upload/".$not_imagen_carpeta."".$not_imagen;
 				<div class="col-md-9 col-sm-12 clearfix">
 					<!--POST-->
 					<article class="post mid fullwidth">
+						<div class="info">
+							<h1><?php echo $not_titulo; ?></h1>
+							<div class="text">
+								<?php echo $not_contenido_corto; ?>
+							</div>
+							<div class="data">
+								<p class="details"><?php echo $cat_titulo; ?> | <?php echo $nota_fechaFinal; ?><span class="redaccion"><?php echo $not_redaccion; ?></span></p>
+							</div>								
+						</div>
+
 						<?php if($not_video<>""){ ?>
 						<div class="video">
 							<iframe width="100%" height="500px" src="//www.youtube.com/embed/<?php echo $not_video; ?>?rel=0" frameborder="0" allowfullscreen></iframe>
@@ -97,16 +127,14 @@ $not_web_img=$web."imagenes/upload/".$not_imagen_carpeta."".$not_imagen;
 							</div>
 						</div>	
 						<?php }elseif($num_notaFotos==0){ ?>
-						<img src="<?php echo $not_web_img; ?>" alt="post-image">
+							<?php if($not_imagen<>""){ ?>
+							<img src="<?php echo $not_web_img; ?>" alt="post-image">
+							<?php } ?>
 						<?php } ?>
 
 						<div class="row">
 							<div class="info col-md-12 col-sm-12">
 								<div class="info">
-									<h1><?php echo $not_titulo; ?></h1>
-									<div class="data">
-										<p class="details"><?php echo $nota_fechaFinal; ?> | <?php echo $user_nomCompleto; ?></p>
-									</div>
 									<div class="text">
 										<?php echo $not_contenido; ?>
 									</div>										
@@ -116,6 +144,23 @@ $not_web_img=$web."imagenes/upload/".$not_imagen_carpeta."".$not_imagen;
 
 					</article>
 					<!--END POST-->
+
+					<?php if($not_categoria==7){ ?>
+					<h3>Sobre el Autor</h3>
+					<article class="row mid member">
+						<img src="img/author.jpg" alt="author">
+						<div class="info">
+							<h1><a href="author.html"><?php echo $user_nomCompleto; ?></a></h1>
+							<p class="text"><?php echo $user_descripcion; ?></p>
+							<ul class="social list-inline">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+								<li><a href="#"><i class="fa  fa-tumblr"></i></a></li>
+							</ul>
+						</div>	
+					</article>
+					<?php } ?>
 				
 					<!-- COMENTARIOS -->
 					<div class="row">
