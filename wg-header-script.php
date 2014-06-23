6e9b87cea064cabcbd716dc866a2ddc7ba7233cf
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-06-23 12:28:19 -0500

    Home: separacion de script de css

--- wg-header-script.php
+++ wg-header-script.php
@@ -1,4 +1,6 @@
 <base href="<?php echo $web; ?>"></base>
-<link rel="stylesheet" href="fonts/font-awesome-4.0.3/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.0.3/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700">
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Montserrat:400,700">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
