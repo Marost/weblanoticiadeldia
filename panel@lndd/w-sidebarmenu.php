5e2e82218aa9693d7d046fca9cb8705d8847a82e
MARCO LOPEZ <mlopez@marostdevelopers.com>
2014-06-23 12:29:29 -0500

    Administrador: agregando opciones de reportero ciudadano en menu

--- panel@lndd/w-sidebarmenu.php
+++ panel@lndd/w-sidebarmenu.php
@@ -6,12 +6,12 @@
     
     <!-- Sidebar subnav -->
     <ul class="subNav">
+        <li><a href="<?php echo $url_admin; ?>paginas/reportero-np/lista.php" title=""><span class="icos-frames"></span>Reportero Ciudadano - No Publicados</a></li>
+        <li><a href="<?php echo $url_admin; ?>paginas/reportero-p/lista.php" title=""><span class="icos-frames"></span>Reportero Ciudadano - Publicados</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias/lista.php" title=""><span class="icos-frames"></span>Noticias</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-tag/lista.php" title=""><span class="icos-frames"></span>Noticias - Etiquetas</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/noticias-seccion/lista.php" title=""><span class="icos-frames"></span>Noticias - Secciones</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/columnistas/lista.php" title="" ><span class="icos-frames"></span>Columnistas</a></li>
-        <li><a href="<?php echo $url_admin; ?>paginas/portada/lista.php" title=""><span class="icos-frames"></span>Edición Impresa</a></li>
-        <li><a href="<?php echo $url_admin; ?>paginas/blog/lista.php" title=""><span class="icos-frames"></span>Blog</a></li>
         <li><a href="<?php echo $url_admin; ?>paginas/usuarios/lista.php" title=""><span class="icos-frames"></span>Usuarios</a></li>
     </ul>
 
